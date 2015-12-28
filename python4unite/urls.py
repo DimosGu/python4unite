43b88152d53cd17f2ec8139b9afd603d710f09a2
WangLu <wl1524520@gmail.com>
2015-12-28 13:32:00 +0800

    update app name

--- python4unite/urls.py
+++ python4unite/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 #from python4unite.views import index
-from home.views import index
+from heyi.views import index
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
